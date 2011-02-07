fc6651671bed3ed9f6e9191e48452683667778c6
cncplyr <cncplyr@90d83dd5-3ca5-cff5-9bfd-5cdd6d1b4a6a>
2011-02-07 18:12:52 +0000

    Started analyser for bounding boxes.

---
+++ trunk/src/animationCreator/AnalyserBoundingBoxes.java
@@ -0,0 +1,15 @@
+package animationCreator;
+
+import java.util.List;
+
+public class AnalyserBoundingBoxes {
+
+	public AnalyserBoundingBoxes() {
+
+	}
+
+	public void Analyse(List<int[]> boundingBoxes) {
+
+	}
+
+}
